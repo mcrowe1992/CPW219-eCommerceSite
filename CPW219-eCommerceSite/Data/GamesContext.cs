360e59fc83f5531db3210e18e0908b9463c1d48d
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-03-14 20:41:02 -0700

    Added DB context class

---
+++ CPW219-eCommerceSite/Data/GamesContext.cs
@@ -0,0 +1,15 @@
+﻿using CPW219_eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW219_eCommerceSite.Data
+{
+	public class GamesContext : DbContext
+	{
+		public GamesContext(DbContextOptions<GamesContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Games> Game { get; set; }
+	}
+}
