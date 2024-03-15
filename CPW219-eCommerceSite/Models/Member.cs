0d3876976cb98a22abe6a73822083b72b757e85f
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-03-15 16:15:41 -0700

    Added Register View Model

--- CPW219-eCommerceSite/Models/Member.cs
+++ CPW219-eCommerceSite/Models/Member.cs
@@ -17,4 +17,27 @@ namespace CPW219_eCommerceSite.Models
 
 
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
 }
