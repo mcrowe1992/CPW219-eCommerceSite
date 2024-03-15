e01209269e3f418768c50b3402dbd1197a4435b6
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-03-14 20:12:49 -0700

    reset branch

--- CPW219-eCommerceSite/Models/Games.cs
+++ CPW219-eCommerceSite/Models/Games.cs
@@ -1,9 +1,11 @@
-﻿namespace CPW219_eCommerceSite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_eCommerceSite.Models
 {
 	public class Games
 	{
 		public string Title { get; set; }
-
+		[Required]
 		public double Price { get; set; }
 
 		public int Quantity { get; set; }
