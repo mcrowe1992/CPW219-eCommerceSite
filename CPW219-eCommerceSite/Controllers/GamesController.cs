33af75be7d1403750f7d0c8bdf26e8aaeea23764
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-03-14 21:49:50 -0700

    refactored create page to work with DbCntext and added minimal styling

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -1,13 +1,20 @@
-﻿using CPW219_eCommerceSite.Models;
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class GamesController : Controller
     {
-        
 
-        [HttpGet]
+        private readonly GamesContext _context;
+
+		public GamesController(GamesContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
         public IActionResult CreateWithModelBinding() 
         { 
             return View(); 
@@ -18,7 +25,11 @@ namespace CPW219_eCommerceSite.Controllers
         {  
             if (ModelState.IsValid)
             {
-                return RedirectToAction("Index", "Home");
+                _context.Game.Add(g); //Prepares insert
+                _context.SaveChanges(); //Executes pending insert
+
+                ViewData["Message"] = $"{g.Title} was added successfully!";
+                return View();
             }
 
             return View(g);
