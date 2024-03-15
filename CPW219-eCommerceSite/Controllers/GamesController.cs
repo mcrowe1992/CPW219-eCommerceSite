49a5b94b6798a98ef6a5c0a3062683c08f0a1f55
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-03-14 18:40:17 -0700

    added games model, controller  and create page

---
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,31 @@
+﻿using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: /Games/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // This action is hit after the user submits form data
+        public IActionResult Create(IFormCollection formData) 
+        {
+            Games g = new()
+            {
+
+            };
+
+            // Validate all data
+
+            // Add to database
+
+            // Return a view
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CPW219-eCommerceSite/Models/Games.cs
@@ -0,0 +1,11 @@
+﻿namespace CPW219_eCommerceSite.Models
+{
+	public class Games
+	{
+		public string Title { get; set; }
+
+		public double Price { get; set; }
+
+		public int Quantity { get; set; }
+	}
+}
